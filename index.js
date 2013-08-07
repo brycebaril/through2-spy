de024c653a13a1c3132349e22cb75506cb8d4d8e
Bryce Baril <bryce@ravenwall.com>
2013-08-06 22:16:09 -0700

    Initial version

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = make
+module.exports.ctor = ctor
+
+var through2 = require("through2")
+
+function ctor(options, fn) {
+  if (typeof options == "function") {
+    fn = options
+    options = {}
+  }
+  options._index = 0
+  return through2.ctor(options, function (chunk, encoding, callback) {
+    if (this.options.wantStrings) chunk = chunk.toString()
+    var err = fn.call(this, chunk, encoding)
+    if (err instanceof Error) return callback(err)
+    this.push(chunk)
+    return callback(null)
+  })
+}
+
+function make(options, fn) {
+  return ctor(options, fn)()
+}
